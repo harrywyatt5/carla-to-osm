29dbc6312f16c5a0f01d3ade347f7b0a2e53d2b4
Harry Wyatt <harrywyatt5@outlook.com>
2026-07-10 01:48:25 +0100

    Start progress on OSM serialization

--- src/carla_to_osm/osm_map.py
+++ src/carla_to_osm/osm_map.py
@@ -1,10 +1,12 @@
 from lxml import etree
 
 class OsmMap:
-    def __init__(self):
+    def __init__(self, coord_service, username):
         self._root = etree.Element("osm", version=0.6, generator="carla_to_osm")
         self._ways = set()
         self._nodes = set()
+        self._coord_service = coord_service
+        self._username = username
 
     def add_ways(self, ways):
         for way in ways:
@@ -19,7 +21,13 @@ class OsmMap:
         self._nodes = self._nodes | set(nodes)
 
     def build(self):
-        pass
+        # Generate points as nodes
+        for point in self._nodes:
+            long, lat = self._coord_service.get_globe_coords(point.x, point.y)
+            etree.SubElement(self._root, "node", id=str(point.id), visible="true", lat=str(lat), lon=str(long))
+
+        # Generate ways 
+
 
     def build_and_write(self, dest):
         pass
